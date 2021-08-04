73c4f19c8c1508dfbb2457993fbe63357ca894cb
Cole vJ <colevj0303@gmail.com>
2021-08-04 14:24:12 +0100

    FAQs 1st draft

--- lib/screens/settings/faqs.dart
+++ lib/screens/settings/faqs.dart
@@ -9,7 +9,107 @@ class FAQs extends StatefulWidget {
 }
 
 class _FAQsState extends State<FAQs> {
-  List<bool> isExpanded = [false, false, false, false, false];
+  List<bool> isExpanded = [false, false, false, false, false, false];
+  List<Text> questions = [
+    Text(
+      'What is a contract?',
+      style: TextStyle(
+          color: Colors.grey[700],
+          height: 1.5,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+    ),
+    Text(
+      'What is a team contract?',
+      style: TextStyle(
+          color: Colors.grey[700],
+          height: 1.5,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+    ),
+    Text(
+      'What is a player contract?',
+      style: TextStyle(
+          color: Colors.grey[700],
+          height: 1.5,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+    ),
+    Text(
+      'How do we calculate prices?',
+      style: TextStyle(
+          color: Colors.grey[700],
+          height: 1.5,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+    ),
+    Text(
+      'Delete profile.',
+      style: TextStyle(
+          color: Colors.grey[700],
+          height: 1.5,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+    ),
+    Text(
+      'Contact us.',
+      style: TextStyle(
+          color: Colors.grey[700],
+          height: 1.5,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+    )
+  ];
+  List<Text> answers = [
+    Text(
+      'A contract is an agreement entered willingly by two parties whereby one party (the user) will receive payment from the other party (Sportfolios Ltd), provided the conditions stipulated in the contract are satisfied.',
+      style: TextStyle(
+          color: Colors.grey[700],
+          height: 1.5,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+    ),
+    Text(
+      'A team contract (Long, Short, Binary, or Custom) is a contract whereby the user will receive payment at expiration (end of the season), provided the team\'s final position satisifies at least one of conditions in the contract.',
+      style: TextStyle(
+          color: Colors.grey[700],
+          height: 1.5,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+    ),
+    Text(
+      'A player contract (Long or Short) is a contract whose final value and payout depends upon the final finishing position of the player based on points accumulated throughout the season amongst all other available players.',
+      style: TextStyle(
+          color: Colors.grey[700],
+          height: 1.5,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+    ),
+    Text(
+      'Prices are calculated using Logarithmic Market Scoring Rule (LMSR). This scoring rule is used by numerous entities such as Augur, The Good Judgement Project, Inkling Markets, Cultivate Labs, and many others. The price calculated depends upon the quantity requested and the quantity already purchased by the community and as such the price will not be same as the sum of individual purchases. The user may be interest to learn more at https://www.cultivatelabs.com/prediction-markets-guide/how-does-logarithmic-market-scoring-rule-lmsr-work.',
+      style: TextStyle(
+          color: Colors.grey[700],
+          height: 1.5,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+    ),
+    Text(
+      'If the user wishes to delete their profile, one need only uninstall the application and email Sportfolios at admin@sportfolios.co.uk with your username and email address with which you registered an account.',
+      style: TextStyle(
+          color: Colors.grey[700],
+          height: 1.5,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+    ),
+    Text(
+      'The Sportfolios administration team can be contacted at: \n\n email: admin@sportfolios.co.uk \n mobile: 0800 555 5555',
+      style: TextStyle(
+          color: Colors.grey[700],
+          height: 1.5,
+          fontSize: 14,
+          fontWeight: FontWeight.normal),
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -30,18 +130,21 @@ class _FAQsState extends State<FAQs> {
                 isExpanded[index] = !itemIsExpanded;
               });
             },
-            children: range(5)
+            children: range(6)
                 .map<ExpansionPanel>(
                   (int i) => ExpansionPanel(
                     isExpanded: isExpanded[i],
-                    headerBuilder: (context, expnded) {
+                    headerBuilder: (context, expanded) {
                       return Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Text('item ${i}'),
+                        child: questions[i],
                       );
                     },
-                    body: Container(
-                      child: Text('hey'),
+                    body: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: answers[i],
+                      ),
                     ),
                   ),
                 )
